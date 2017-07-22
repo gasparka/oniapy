aa90946408652a8e3a798297f3317a2ef47f32f9
petspats <gkarm@live.com>
2017-07-22 22:36:48 +0300

    working hello2

--- oniapy/hello.py
+++ oniapy/hello.py
@@ -2,15 +2,17 @@
 from math import cos, sin
 
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, Range1d
+from bokeh.layouts import layout
+from bokeh.models import ColumnDataSource, Range1d, Button, BoxSelectTool
 from bokeh.plotting import figure
+from copy import deepcopy
 
+p = figure(plot_width=1024, plot_height=512)
 
-p = figure(x_range=(0, 10), plot_width=1024, plot_height=512)
-
-# this is the data source we will stream to
 source = ColumnDataSource(data=dict(x=[1], y=[0]))
 r = p.rect(x='x', y='y', width=17, height=50, alpha=0.5, color="red", source=source, width_units="screen", height_units="screen")
+# r = p.rect(x=0, y=0, width=17, height=50, alpha=0.5, color="red", width_units="screen", height_units="screen")
+
 
 
 class Update:
@@ -18,25 +20,34 @@ class Update:
         self.c = 0
 
     def __call__(self, *args, **kwargs):
-        print(r.data_source.data["y"])
-        r.data_source.data["y"][0] += 0.1
-        r.update()
-        # source.data['y'][-1] = source.data['y'][-1] + 0.1
-        # print(source.data['y'][-1])
-        # print('wtf')
-        # construct the new values for all columns, and pass to stream
-        # self.c += 0.1
-        # new_data = dict(x=[1], y=[self.c])
-        # p.x_range = Range1d(self.c, self.c+100)
-        # p.y_range.start = self.c - 10
-        # p.y_range.end = self.c + 10
-        # source.data.update()
-        # source.stream(new_data)
+        # print(r.data_source.data["y"])
+
+        # source.data = dict(x=[1], y=[self.c+10])
+        p.y_range.start = 0 + self.c
+        p.y_range.end = 100 + self.c
+        # p.y_range.bounds = (p.y_range.start, p.y_range.end)
+        # p.rect(x='x', y='y', width=17, height=self.c, alpha=0.5, color="red", source=source, width_units="screen",
+        #        height_units="screen")
+        # r.data_source.data["y"] = self.c
+        self.c -= .1
+
+# create a callback that will add a number in a random location
+def callback():
+    p.rect(x=1, y=p.y_range.start-20, width=17, height=50, alpha=0.5, color="red", width_units="screen",
+               height_units="screen")
+
+# add a button widget and configure with the call back
+button = Button(label="The KEY")
+button.on_click(callback)
+
 
 
 curdoc().add_periodic_callback(Update(), 10)
-curdoc().add_root(p)
+lay_out=layout([[p], [button]])
+curdoc().add_root(lay_out)
 
+if __name__ == '__main__':
+    d = Update()()
 # def update():
 #     x, y = source.data['x'][-1], source.data['y'][-1]
 #
---
+++ oniapy/hello2.py
@@ -0,0 +1,97 @@
+''' Present an interactive function explorer with slider widgets.
+
+Scrub the sliders to change the properties of the ``sin`` curve, or
+type into the title text box to update the title of the plot.
+
+Use the ``bokeh serve`` command to run the example by executing:
+
+    bokeh serve sliders.py
+
+at your command prompt. Then navigate to the URL
+
+    http://localhost:5006/sliders
+
+in your browser.
+
+'''
+import numpy as np
+
+from bokeh.io import curdoc
+from bokeh.layouts import row, widgetbox
+from bokeh.models import ColumnDataSource, Button
+from bokeh.models.widgets import Slider, TextInput
+from bokeh.plotting import figure
+
+# Set up data
+N = 200
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+source = ColumnDataSource(data=dict(x=x, y=y))
+
+
+# Set up plot
+plot = figure(plot_height=400, plot_width=400, title="my sine wave",
+              tools="crosshair,pan,reset,save,wheel_zoom",
+              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
+
+plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
+
+
+# Set up widgets
+text = TextInput(title="title", value='my sine wave')
+offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
+amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0)
+phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
+freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1)
+
+
+# Set up callbacks
+def update_title(attrname, old, new):
+    plot.title.text = text.value
+
+text.on_change('value', update_title)
+
+class Update:
+    def __init__(self):
+        self.c = 0
+
+    def __call__(self, *args, **kwargs):
+        plot.y_range.start = 0 + self.c
+        plot.y_range.end = 100 + self.c
+        self.c -= .1
+
+def update_data(attrname, old, new):
+
+    # Get the current slider values
+    a = amplitude.value
+    b = offset.value
+    w = phase.value
+    k = freq.value
+
+    # Generate the new curve
+    x = np.linspace(0, 4*np.pi, N)
+    y = a*np.sin(k*x + w) + b
+
+    source.data = dict(x=x, y=y)
+
+for w in [offset, amplitude, phase, freq]:
+    w.on_change('value', update_data)
+
+# create a callback that will add a number in a random location
+def callback():
+    plot.rect(x=1, y=plot.y_range.start-20, width=17, height=50, alpha=0.5, color="red", width_units="screen",
+               height_units="screen")
+
+# add a button widget and configure with the call back
+button = Button(label="The KEY")
+button.on_click(callback)
+
+
+# Set up layouts and add to document
+inputs = widgetbox(text, offset, amplitude, phase, freq)
+
+
+
+curdoc().add_periodic_callback(Update(), 10)
+curdoc().add_root(row(inputs, plot, button, width=800))
+curdoc().title = "Sliders"
